ed312c5fddc9ecffc6bfe1371c8a97393fbf8eac
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-07-30 21:22:01 +0700

    make click on small-img

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ if (close) {
     })
 }
  /* How to make humberger */
+
+// how to make click on small-img
+
+const mainImg = document.getElementById('MainImg');
+const smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src;
+}
+
+// how to make click on small-img
